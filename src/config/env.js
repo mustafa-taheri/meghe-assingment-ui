c37140abe4d24a047177bb17a786e7d02faaaeb8
mustafa-taheri <muzimax5252@gmail.com>
2021-11-10 15:30:38 +0530

    endpoints added

---
+++ src/config/env.js
@@ -0,0 +1,8 @@
+let base_url = 'http://localhost:5001'
+
+export const api_enpoint = {
+    base_url: base_url,
+    refresh: '/auth/refreshToken',
+    login: '/auth/login',
+    register : '/user/register'
+}
